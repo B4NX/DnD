9bf74926f35245f6ba4c78c6045816b403b912e1
Nikko <nikko.rush@gmail.com>
2013-11-09 16:57:53 -0800

    I do thing

--- DnD-Client/ClientUI.cs
+++ DnD-Client/ClientUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace DnD_Client
         public ClientUI()
         {
             InitializeComponent();
+            
         }
     }
 }
