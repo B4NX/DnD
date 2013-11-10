a3bf178ce563ca33dc96f756e5641e38c15b7628
Nikko <nikko.rush@gmail.com>
2013-11-09 19:09:41 -0800

    Revert "Networking things"
    This reverts commit 35fab6bfdcee86c9833f2cc9ae3b0cc4959e51b4.

--- Networking/Send.cs
+++ Networking/Send.cs
@@ -7,13 +7,16 @@ using System.Net;
 using System.Net.Sockets;
 using System.IO;
 
-namespace Networking {
-    public class Send {
+namespace Networking
+{
+    public class Send
+    {
         private static TcpClient client = new TcpClient();
-        public static void Run() {
+        public static void Run()
+        {
             client.Connect("127.0.0.1", 666);
             NetworkStream stream = client.GetStream();
-            StreamReader streamReader = new StreamReader(stream);
+            StreamReader streamReader= new StreamReader(stream);
             StreamWriter streamWriter = new StreamWriter(stream);
             streamWriter.Write("Banana");
         }
