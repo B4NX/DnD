3921e8a4e35531f080e1374d074e1b0b4e6fe07b
Nikko <nikko.rush@gmail.com>
2013-11-10 07:32:41 -0800

    New and Improved Message class, now only $9.99

--- Networking/Message.cs
+++ Networking/Message.cs
@@ -5,8 +5,8 @@ using System.Text;
 
 namespace Networking {
     public class Message {
-        public static Message EMPTY=new Message(HeaderVal.EMPTY);
-        public enum HeaderVal {
+        private byte[] mssg = new byte[256];
+        public enum Head {
             EMPTY,
             STAT,
             LOG,
@@ -15,65 +15,26 @@ namespace Networking {
             MAP,
             NPC
         };
-        private byte[] mssg;
-        private HeaderVal header;
-        //First Byte is the header to indicate what the message means
-        public Message() {
-            mssg = new byte[256];
+        public Message(Head h) {
+            this.mssg[0] = (byte)(int)h;
         }
-        public Message(HeaderVal hv){
-            if (hv==HeaderVal.EMPTY){
-                mssg=new byte[256];
+        public static void getLogMessage(string s) {
+            Message tmp=new Message(Head.LOG);
+            byte[] msgArray=ToByteArray(s);
+            for (int i = 0; i <= msgArray.GetLength(0) - 1; i++) {
+                tmp.mssg[i + 1] = msgArray[i];
             }
+            Console.WriteLine(tmp);
         }
-        public Message(byte[] b) {
-            this.header = (HeaderVal)b[0];
-            this.mssg = b;
-        }
-        public static Message getLogMessage(string message) {
-            Message temp = new Message();
-            temp.header = HeaderVal.LOG;
-            temp.mssg[0] = 2;
-            byte[] tempMess = ToByteArray(message);
-            for (int i = 0; i <= tempMess.Length - 1;++i) {
-                temp.mssg[i+1] = tempMess[i];
-            }
-            return temp;
-        }
-        public static implicit operator byte[](Message m){
-            return m.mssg;
-        }
-        public static implicit operator Message(byte[] b) {
-            Message tmp = new Message();
-            tmp.header = (HeaderVal)b[0];
-            tmp.mssg = b;
-            return tmp;
-        }
-        private static byte[] ToByteArray(string s) {
+        public static byte[] ToByteArray(string s) {
             return new UnicodeEncoding().GetBytes(s);
         }
-        private static string ByteToString(byte[] b) {
-            char[] x = new Char[b.GetLength(0)];
-            for (int i = 0; i <= b.GetLength(0) - 1; i++) {
-                x[i] = (char)b[i];
-            }
-            return new String(x);
-        }
         public override string ToString() {
-            string s="";
-            for (int i = 1; i <= this.mssg.Length - 1; i++) {
-                if (this.mssg[i] != 0) {
-                    s += (char)(this.mssg[i]);
-                    continue;
-                }
+            string s = "";
+            for (int i = 1; i <= this.mssg.GetLength(0)-1; i++) {
+                s += (char)this.mssg[i];
             }
             return s;
-        }
-
-        public HeaderVal Header {
-            get {
-                return this.header;
-            }
         }
     }
 }
