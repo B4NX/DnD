917289c8779d7de9c8772bc447692b5b94c4a65d
Nikko <nikko.rush@gmail.com>
2013-11-09 21:21:03 -0800

    Networking Communication Test

---
+++ Networking/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Networking {
+    class Main {
+        public static void Main(string[] args) {
+            Console.Write("S/C: ");
+            string ans=Console.ReadLine();
+            if (ans.ToUpper() == "S") {
+                Console.WriteLine("Server");
+                Server.init();
+            } else if (ans.ToUpper() == "C") {
+                Console.WriteLine("Client");
+                Client.init();
+            }
+        }
+    }
+}
---
+++ Networking/Server.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace Networking {
+    class Server {
+        public static void init() {
+
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 666);
+            sock.Bind(ep);
+            sock.Listen(Int32.MaxValue);
+            Debug.WriteLine("Waiting for connection");
+            Socket client;
+            while (true) {
+                try {
+                    client = sock.Accept();
+                    break;
+                } catch (SocketException e) {
+                    Debug.WriteLine(e.Message);
+                    continue;
+                }
+            }
+            Debug.WriteLine("Connected!");
+            byte[] b = new byte[256];
+            client.Receive(b);
+            Console.WriteLine("Please say something nice to your \"friend\".");
+            string s=Console.ReadLine();
+            client.Send(ToByteArray(s));
+            while (true) ;
+            
+        }
+        private static void WriteByeArray(byte[] b) {
+            foreach (byte x in b) {
+                if (x != 0) {
+                    Console.Write((char)x);
+                }
+            }
+        }
+        private static byte[] ToByteArray(string s) {
+            return new UnicodeEncoding().GetBytes(s);
+        }
+    }
+}
