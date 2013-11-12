2597d4c62215e79ebcdbd56361f0ba24e3947c25
p0rtalplayer <s-achronister@487M009503.lwsd.k12wa.org>
2013-11-12 11:04:14 -0800

    fixed the goddamn enter button

--- DnD-Client/ClientConnectUI.Designer.cs
+++ DnD-Client/ClientConnectUI.Designer.cs
@@ -104,6 +104,7 @@
             // 
             // ClientConnectUI
             // 
+            this.AcceptButton = this.connectBtn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
