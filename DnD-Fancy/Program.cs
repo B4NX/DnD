5c047050dca5d2dcba90a0299cfd1695772382f6
p0rtalplayer <s-achronister@487M009503.lwsd.k12wa.org>
2013-11-13 12:25:52 -0800

    Started framework for OpenGL based interface

---
+++ DnD-Fancy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using OpenTK;
+using SharpGLass;
+
+namespace DnD_Fancy
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Engine Game = new Engine();
+        }
+    }
+}
